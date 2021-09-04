8088cfa26fcba31dcc2b505378aa9c467ddca8c7
Desnes A. Nunes do Rosario <desnesn@linux.ibm.com>
2021-09-03 21:34:40 -0300

    ppc64le_selftests: fix timeout dumped core error
    Signed-off-by: Desnes A. Nunes do Rosario <desnesn@linux.ibm.com>

--- ppc64le_selftests/runtest.sh
+++ ppc64le_selftests/runtest.sh
@@ -37,6 +37,7 @@ rlJournalStart
 
 		rlRun "yum -y install asciidoc audit-libs-devel bc binutils-devel bison clang ctags dnf-utils elfutils-devel elfutils-libelf-devel flex gcc git glibc-static hmaccalc java-devel kabi-dw kernel-debug kernel-debug-debuginfo libcap-devel libcap-ng-devel libmnl-devel llvm llvm-toolset m4 make ncurses-devel net-tools newt-devel numactl-devel openssl openssl-devel pciutils-devel perl-devel perl-Ext* perl\(ExtUtils::Embed\) perl-generators python3-devel python3-docutils rpm-build rsync tmux vim wget xmlto xz-devel zlib-devel"
 
+		# Setup the src repo
 		if [ ! -f /etc/yum.repos.d/beaker-BaseOS-source.repo ]; then
 			cp /etc/yum.repos.d/beaker-BaseOS.repo /etc/yum.repos.d/beaker-BaseOS-source.repo
 			sed -i 's|ppc64le\/os|source\/tree|g' /etc/yum.repos.d/beaker-BaseOS-source.repo
@@ -45,8 +46,9 @@ rlJournalStart
 
 		rlRun "lscpu" 0 "Log CPU info"
 		rlShowRunningKernel
+		rlRun "if [ -x /usr/bin/timeout ]; then mv /usr/bin/{,.}timeout ; fi" 0 "Conceal timeout"
 
-		# now we need the kernel src rpm
+		# Fetch the kernel src rpm
 		IFS=- read -a UNAME <<<$(uname -r)
 		ARCH=$(uname -i)
 		VERSION=${UNAME[0]}
@@ -55,17 +57,21 @@ rlJournalStart
 		rlRun "yumdownloader --source kernel" 0 "Downloading kernel source rpm"
 		
 		rlRun "rpm -ivh kernel-${VERSION}-${RELEASE}.src.rpm" 0 "Installing kernel source rpm"
-		cd $HOME/rpmbuild/SOURCES
+		pushd $HOME/rpmbuild/SOURCES
 		tar -xf linux-${VERSION}-${RELEASE}.tar.xz
+
 	rlPhaseEnd
 	
 	rlPhaseStartTest
-		cd linux-${VERSION}-${RELEASE}/
+
+		pushd linux-${VERSION}-${RELEASE}/
 
 		# SELFTESTSLOG=$(mktemp /mnt/testarea/selftests.XXXXXX)
-		SELFTESTSLOG=/mnt/testarea/selftests.output
+		SELFTESTSLOG=/mnt/testarea/selftests.log
+
 		SELFTESTSPASS=/mnt/testarea/selftests.pass
 		SELFTESTSFAIL=/mnt/testarea/selftests.fail
+		SELFTESTSSKIP=/mnt/testarea/selftests.skip
 
 		# MAKELEVEL=0 is needed to fool the kernel's tools/testing/selftests/lib.mk
 		#   ifeq (0,$(MAKELEVEL))
@@ -74,38 +80,49 @@ rlJournalStart
 		# A Beaker task starts with 'make run', thus MAKELEVEL=1 for the selftests,
 		# thus OUTPUT is not set, and the whole build fails.
 
-		rlRun "make -C tools/testing/selftests/powerpc run_tests MAKELEVEL=0 >>$SELFTESTSLOG 2>&1" 0 "Running powerpc selftests"
-		
+		# Run selftests
+		rlRun "make MAKELEVEL=0 -C tools/testing/selftests/powerpc run_tests >>$SELFTESTSLOG 2>&1" 0 "Running powerpc selftests"
+
 		CPU=$(lscpu | grep "Model name" | awk '{ print $3 }' | sed 's/,//')
+		echo >> $SELFTESTLOG
 		echo $CPU >> $SELFTESTLOG
+		echo >> $SELFTESTLOG
 
 		HOSTNAME=$(hostname)
+		echo >> $SELFTESTLOG
 		echo $HOSTNAME >> $SELFTESTLOG
+		echo >> $SELFTESTLOG
 
-		PASSED=$(grep -c 'PASS' $SELFTESTSLOG)
-		FAILED=$(grep -c 'FAIL' $SELFTESTSLOG)
-		SKIPPED=$(grep -c 'SKIP' $SELFTESTSLOG)
+		N_PASSED=$(grep -c 'PASS' $SELFTESTSLOG)
+		N_FAILED=$(grep -c 'FAIL' $SELFTESTSLOG)
+		N_SKIPPED=$(grep -c 'SKIP' $SELFTESTSLOG)
 		
 		rlAssert0 "Assert 0 tests failed" $FAILED
 		
 		rlLog "$CPU"
 		rlLog "$HOSTNAME"
-		rlLog "Passed tests: $PASSED"
-		rlLog "Failed tests: $FAILED"
-		rlLog "Skipped tests: $SKIPPED"
+		rlLog "Passed tests: $N_PASSED"
+		rlLog "Failed tests: $N_FAILED"
+		rlLog "Skipped tests: $N_SKIPPED"
 		
+		grep "^ok" $SELFTESTSLOG | awk '{print $3$4" "$5}' >> $SELFTESTSPASS
+		grep "^not ok" $SELFTESTSLOG | awk '{print $4$5" "$6" "$7$8" "$9}' >> $SELFTESTSFAIL
+		grep "skip:" $SELFTESTSLOG | awk '{print $3}' >> $SELFTESTSSKIP
+
 		rlFileSubmit $SELFTESTSLOG SELFTESTS.LOG
-
-		cat $SELFTESTSLOG | grep PASS > $SELFTESTSPASS
-		cat $SELFTESTSLOG | grep FAIL > $SELFTESTSFAIL
-
 		rlFileSubmit $SELFTESTSPASS SELFTESTS.PASS
 		rlFileSubmit $SELFTESTSFAIL SELFTESTS.FAIL
+
+		popd
+
 	rlPhaseEnd
 	
 	rlPhaseStartCleanup
-		#rm -fr $HOME/rpmbuild
-		rlLog "== End of powerpc selftest execution =="
+
+		popd
+
+		rlRun "if [ -x /usr/bin/.timeout ]; then mv /usr/bin/{.,}timeout ; fi" 0 "Unconceal timeout"
+
 	rlPhaseEnd
 
 	rlJournalPrintText
